4bf36341208f6ab4d6a5270cefbcdb9ba2ab3ff4
code-crunch07 <167286290+code-crunch07@users.noreply.github.com>
2026-02-25 15:21:22 +0530

    fixed weight issue/annoucemts ad5D

--- client/app/shop/page_backup.tsx
+++ client/app/shop/page_backup.tsx
@@ -24,6 +24,7 @@ interface Product {
     _id: string;
     name: string;
   };
+  freeShipping?: boolean;
 }
 
 interface Category {
--- client/components/ProductCarousel.tsx
+++ client/components/ProductCarousel.tsx
@@ -24,6 +24,7 @@ interface Product {
   productCategory?: {
     name: string;
   };
+  freeShipping?: boolean;
 }
 
 type DataSource = 'popular' | 'newest';
